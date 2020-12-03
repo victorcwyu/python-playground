dee463e55cd72ecdf88a1178bd2c8d78189c98f1
Victor <victorcwyu@gmail.com>
2020-12-03 14:55:54 -0500

    Create index view

--- flask-tutorial/flaskr/blog.py
+++ flask-tutorial/flaskr/blog.py
@@ -5,3 +5,14 @@ from flaskr.auth import login_required
 from flaskr.db import get_db
 
 bp = Blueprint("blog", __name__)
+
+# show all of the posts, most recent first
+@bp.route("/")
+def index():
+    db = get_db()
+    posts = db.execute(
+        "SELECT p.id, title, body, created, author_id, username"
+        " FROM post p JOIN user u ON p.author_id = u.id"
+        " ORDER BY created DESC"
+    ).fetchall()
+    return render_template("blog/index.html", posts=posts)
